25a9a264593a111f04fd9cea38d4c4a5fcbac174
Itay <itay.olivcovitz@gmail.com>
2022-05-10 16:59:04 +0300

    branch

---
+++ Branch.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "Branch.h"
+#include "Item.h"
+
+//constractor
+Branch::Branch(std::string location)
+	: m_location(location),m_capacity(0)
+{
+	this->m_item_Catalog = new Item * [STORE_SIZE];
+}
+
+//check the index for the next item
+int Branch:: store() 
+{
+	if (this->m_capacity >= STORE_SIZE)
+	{
+		delete this->m_item_Catalog[m_capacity % 10];
+		return m_capacity % 10;
+
+	}
+	return this->m_capacity;
+} 
+
+//return how many items in the catalog
+int Branch:: howManyItems()const
+{
+	if (this->m_capacity >= STORE_SIZE)
+	{
+		return STORE_SIZE;
+	}
+	return this->m_capacity;
+}
+
+// add item to the catalog
+void Branch::addItem(const Item* item)
+{
+	int index = store();
+	
+	this->m_item_Catalog[index] = item->clone();
+	this->m_capacity++;
+}
+
+// return the catalog
+Item** Branch::getCatalog(int &num)  
+{
+	num = howManyItems();
+	return this->m_item_Catalog;
+}
+
+//set location
+void Branch::setLocation(const std::string location)
+{
+	this->m_location = location;
+}
+//return location
+std::string Branch::getLocation() const
+{
+	return this->m_location;
+}
+
+//destractor
+Branch::~Branch()
+{
+	int length = howManyItems();
+	for (int i = 0; i < length; i++)
+	{
+		delete this->m_item_Catalog[i];
+	}
+}
---
+++ Branch.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Item.h"
+#define STORE_SIZE 10
+class Branch 
+{
+	Item** m_item_Catalog;
+	std::string m_location;
+	int m_capacity;
+
+	//check the index for the next item
+	int store();
+
+	// return how many items in the catalog
+	int howManyItems()const ;
+
+public:
+	//constractor
+	Branch(std::string location);
+
+	//addItem to Item catalog
+	void addItem(const Item * item);
+
+	//return item catalog
+	Item** getCatalog(int & num) ;
+	
+	//set location
+	void setLocation(const std::string location) ;
+
+	//return location
+	std::string getLocation() const;
+
+	//destractor
+	~Branch();
+
+};

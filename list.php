4d04282763c691faba310a1702672c9ca0fb4167
NullThrall <121239426+NullThrall@users.noreply.github.com>
2023-07-22 17:01:55 +0300

    Add files via upload

--- list.php
+++ list.php
@@ -39,7 +39,7 @@ function getUserNameByID($userID)
   <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
   <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-  <title>Taskmaster - example list</title>
+  <title>Taskmaster - <?php echo $userFullName; ?>'s list</title>
   <style>
     .custom-autocomplete {
       position: absolute;
